bd78ffc04c016479d2e04957f6be59c1f7e1adca
kapil <kapilkumar1191@gmail.com>
2018-07-05 17:10:48 +0530

    rubocop changes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,54 +1,54 @@
+# login via oauth controller
 class LoginController < ActionController::Base
+  def index
+    @client_id = ENV['client_id']
+  end
 
-  	def index
-  		@client_id = ENV['client_id']
-  	end
-
-	def callback_handling
-	  	@client_id = ENV['client_id']
-	  	@client_secret = ENV['client_secret']
-	  	@timeout_flag = false
-	  	@request_failure = false
-	  	require 'net/https'
-	    require 'open-uri'
-	    require 'uri'
-	    require 'cgi'
-	    require 'timeout'
-	        begin
-	            timeoutstatus = Timeout::timeout(50) {
-				    uri = URI.parse(URI::encode("https://github.com/login/oauth/access_token?client_id=#{@client_id}&client_secret=#{@client_secret}&code=#{params['code']}"))
-				    http = Net::HTTP.new(uri.host, uri.port)
-				    http.use_ssl = true
-				    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-				    request = Net::HTTP::Post.new(uri.request_uri)
-				    response = http.request(request)
-		    		response_body = CGI::parse(response.body)
-		    		Rails.logger.error "********#{response.code}*****#{response_body}*******"
-		    		if ((response.code == '200') and (response_body.key?'access_token'))
-				    	uri = URI.parse(URI::encode("https://api.github.com/user/repos?access_token=#{response_body['access_token'].last}"))
-				    	http = Net::HTTP.new(uri.host, uri.port)
-					    http.use_ssl = true
-					    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-					    request = Net::HTTP::Get.new(uri.request_uri)
-					    response_1 = http.request(request)
-					    Rails.logger.error "*********#{response_1.code}********#{response_1.body}***"
-					    @repos_hash = {}
-					    if response_1.code == '200'
-					    	body_hash = JSON.parse(response_1.body)
-					    	body_hash.each_with_index{|body,i|
-					    		local_hash = {}
-					    		local_hash['name'] = body['name']
-					    		local_hash['description'] = body['description']
-					    		local_hash['private'] = body['private']
-					    		@repos_hash[i] = local_hash
-					    	}
-					    end
-				    else
-				    	@request_failure = true
-				    end
-	    		}
-	    	rescue Timeout::Error
-	            @timeout_flag = true
-	        end    
-	end
+  def callback_handling
+    @client_id = ENV['client_id']
+    @client_secret = ENV['client_secret']
+    @timeout_flag = false
+    @request_failure = false
+    require 'net/https'
+    require 'open-uri'
+    require 'uri'
+    require 'cgi'
+    require 'timeout'
+    begin
+        Timeout.timeout(50) do
+         uri = URI.parse(URI.encode("https://github.com/login/oauth/access_token?client_id=#{@client_id}&client_secret=#{@client_secret}&code=#{params['code']}"))
+         http = Net::HTTP.new(uri.host, uri.port)
+         http.use_ssl = true
+         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+         request = Net::HTTP::Post.new(uri.request_uri)
+         response = http.request(request)
+         response_body = CGI.parse(response.body)
+         Rails.logger.error "********#{response.code}*****#{response_body}*******"
+         if (response.code == '200') && (response_body.key? 'access_token')
+           uri = URI.parse(URI.encode("https://api.github.com/user/repos?access_token=#{response_body['access_token'].last}"))
+           http = Net::HTTP.new(uri.host, uri.port)
+           http.use_ssl = true
+           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+           request = Net::HTTP::Get.new(uri.request_uri)
+           response_1 = http.request(request)
+           Rails.logger.error "*********#{response_1.code}********#{response_1.body}***"
+           @repos_hash = {}
+           if response_1.code == '200'
+             body_hash = JSON.parse(response_1.body)
+             body_hash.each_with_index do |body, i|
+               local_hash = {}
+               local_hash['name'] = body['name']
+               local_hash['description'] = body['description']
+               local_hash['private'] = body['private']
+               @repos_hash[i] = local_hash
+             end
+           end
+         else
+           @request_failure = true
+         end
+        end
+    rescue Timeout::Error
+       @timeout_flag = true
+    end
+  end
 end
