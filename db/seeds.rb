1e5103b0f18d85a6fe89a11942ec6c63a790b106
kapil <kapilkumar1191@gmail.com>
2018-07-25 23:48:44 +0530

    seed commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,94 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+states=['andaman nicobar',
+'andhra pradesh',
+'arunachal pradesh',
+'assam',
+'bihar',
+'chandigarh',
+'chhattisgarh',
+'dadra nagar haveli',
+'daman diu',
+'delhi',
+'goa',
+'gujarat',
+'haryana',
+'himachal pradesh',
+'jammu kashmir',
+'jharkhand',
+'karnataka',
+'kerala',
+'lakshadweep',
+'madhya pradesh',
+'maharashtra',
+'manipur',
+'meghalaya',
+'mizoram',
+'nagaland',
+'odisha',
+'puducherry',
+'punjab',
+'rajasthan',
+'sikkim',
+'tamil nadu',
+'telangana',
+'tripura',
+'uttar pradesh',
+'uttarakhand',
+'west bengal']
+
+states.each{|state|
+	State.create! :name => state
+}
+
+cities = ['ajmer',
+'alwar',
+'banswara',
+'baran',
+'barmer',
+'bharatpur',
+'bhilwara',
+'bikaner',
+'bundi',
+'chittorgarh',
+'churu',
+'dausa',
+'dholpur',
+'dungarpur',
+'ganganagar',
+'hanumangarh',
+'jaipur',
+'jaisalmer',
+'jalore',
+'jhalawar',
+'jhunjhunu',
+'jodhpur',
+'karauli',
+'kota',
+'nagaur',
+'pali',
+'pratapgarh',
+'rajsamand',
+'sawai madhopur',
+'sikar',
+'sirohi',
+'tonk',
+'udaipur']
+
+cities.each{|ct|
+	City.create! :state_id => State.find_by_name('rajasthan').id, :name => ct
+}
+
+State.all.each{|st|
+cities = st.cities
+cities.each{|city|
+ii = 1
+while ( ii < 6) do
+name = 'package_'+ii.to_s+'_'+city.name
+Package.create! :package_name => name, :city_id => city.id
+ii = ii+1
+end
+}
+}
