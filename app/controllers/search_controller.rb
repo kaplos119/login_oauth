84645f95338fff2a6d322a67d131bfd1370eb07c
kaplos119 <40788762+kaplos119@users.noreply.github.com>
2018-08-21 12:36:27 +0530

    cached all states data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ActionController::Base
 	def index
-		@all_states = State.select(:id,:name)			
+		Rails.cache.fetch("all_states",expires_in: 1.day)  do
+		 @all_states = State.select(:id,:name)
+		end
+		puts '*******#{@all_states}********'
 	end
 
 	def result
