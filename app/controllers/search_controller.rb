fa47f0db0ebafbb2085a0d5435a098890f880edc
kaplos119 <40788762+kaplos119@users.noreply.github.com>
2018-08-21 17:36:19 +0530

    update variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ActionController::Base
 	def index
-		Rails.cache.fetch("all_states",expires_in: 1.day)  do
+		@all_states = Rails.cache.fetch("all_states",expires_in: 1.day)  do
 		 State.select(:id,:name)
 		end
-		@all_states = Rails.cache.fetch('all_states')
 	end
 
 	def result
@@ -11,11 +10,9 @@ class SearchController < ActionController::Base
 		state = State.find_by_id(state_id)
 		@packages = {}
 		unless state.blank?
-			Rails.logger.error "*******package_#{state_id}_#{params[:city].downcase}*****"
-			Rails.cache.fetch("package_#{state_id}_#{params[:city].downcase}",expires_in: 30.minutes) do
+			@cities = Rails.cache.fetch("package_#{state_id}_#{params[:city].downcase}",expires_in: 30.minutes) do
 			 state.cities.select(:id,:name).where("name like ?","#{params[:city].downcase}%")
 			end
-			@cities = Rails.cache.fetch("package_#{state_id}_#{params[:city].downcase}")
 			if !@cities.blank?
 				@cities.each{|city|
 					pkgs = city.packages.limit(3)
